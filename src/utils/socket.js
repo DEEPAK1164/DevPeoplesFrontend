4ef0ed56b1e888dfe5aa23d8d7914e9eca634ff8
Deepak <2001dkmaurya@gmail.com>
2025-07-16 17:46:22 +0530

    chat feature added

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,7 +4,13 @@ import { BASE_URL } from "./constant";
 export const createSocketConnection=()=>{
 
     // it is telling client to connect with backend web socket
-return io(BASE_URL);
+    if(location.hostname==="localhost")
+    {
+      return io(BASE_URL);
+    }
+    else{
+      return io("/",{path:"/api/socket.io"});
+    }
 
 
 }
